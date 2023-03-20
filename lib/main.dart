ab6cd017c55817e99786a850bc34a2efdccb7b7c
studAlexP <alexander.pruka@stud.fh-campuswien.ac.at>
2023-03-20 20:13:45 +0100

    Implemented env file loading

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:quiz_it_flutter/presentation/screens/home/home_screen.dart';
 
-void main() {
+Future main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
