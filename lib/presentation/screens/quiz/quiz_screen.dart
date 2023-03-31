6935deaae9e471025cd4d743cbdf332d6484f3d4
studAlexP <alexander.pruka@stud.fh-campuswien.ac.at>
2023-03-31 11:01:35 +0200

    Added QuizScreen

---
+++ lib/presentation/screens/quiz/quiz_screen.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_it_flutter/data/network/quiz_api_impl.dart';
+import 'package:quiz_it_flutter/domain/model/question.dart';
+import 'package:quiz_it_flutter/domain/util/settings.dart';
+import 'package:quiz_it_flutter/presentation/screens/home/home_screen.dart';
+
+class QuizPage extends StatefulWidget {
+  const QuizPage({super.key});
+
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Question>? questions;
+
+  getData() async {
+    questions = await QuizApiImpl().getQuestions(
+        category: Settings.category,
+        difficulty: Settings.difficulty,
+        limit: Settings.limit);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: getData(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return QuizPageContent(questions: questions);
+        } else {
+          return const LoadingIndicator();
+        }
+      },
+    );
+  }
+}
+
+class LoadingIndicator extends StatelessWidget {
+  const LoadingIndicator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Loading"),
+        elevation: 3,
+      ),
+      body: const Center(child: CircularProgressIndicator()),
+    );
+  }
+}
+
+class QuizPageContent extends StatefulWidget {
+  final List<Question>? questions;
+
+  const QuizPageContent({super.key, required this.questions});
+
+  @override
+  _QuizPageContentState createState() => _QuizPageContentState();
+}
+
+class _QuizPageContentState extends State<QuizPageContent> {
+  int currentQuestion = 0;
+  List<bool> isAnswerCorrect = List<bool>.filled(6, false);
+
+  void showAnswers() {
+    setState(() {
+      isAnswerCorrect = [
+        widget.questions![currentQuestion].correctAnswers.answerACorrect ==
+            "true",
+        widget.questions![currentQuestion].correctAnswers.answerBCorrect ==
+            "true",
+        widget.questions![currentQuestion].correctAnswers.answerCCorrect ==
+            "true",
+        widget.questions![currentQuestion].correctAnswers.answerDCorrect ==
+            "true",
+        widget.questions![currentQuestion].correctAnswers.answerECorrect ==
+            "true",
+        widget.questions![currentQuestion].correctAnswers.answerFCorrect ==
+            "true",
+      ];
+    });
+  }
+
+  void nextQuestion() {
+    setState(() {
+      currentQuestion += 1;
+      isAnswerCorrect.fillRange(0, isAnswerCorrect.length, false);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.questions![currentQuestion].category),
+        elevation: 3,
+      ),
+      body: Column(children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Flexible(child: Text(widget.questions![currentQuestion].question))
+          ],
+        ),
+        const SizedBox(
+          height: 50,
+        ),
+        Column(
+          children: [
+            if (widget.questions![currentQuestion].answers.answerA != null)
+              _buildAnswerToggleButtons(
+                  widget.questions![currentQuestion].answers.answerA,
+                  isAnswerCorrect[0],
+                  (value) => setState(() => isAnswerCorrect[0] = value)),
+            const SizedBox(height: 10),
+            if (widget.questions![currentQuestion].answers.answerB != null)
+              _buildAnswerToggleButtons(
+                  widget.questions![currentQuestion].answers.answerB,
+                  isAnswerCorrect[1],
+                  (value) => setState(() => isAnswerCorrect[1] = value)),
+            const SizedBox(height: 10),
+            if (widget.questions![currentQuestion].answers.answerC != null)
+              _buildAnswerToggleButtons(
+                  widget.questions![currentQuestion].answers.answerC,
+                  isAnswerCorrect[2],
+                  (value) => setState(() => isAnswerCorrect[2] = value)),
+            const SizedBox(height: 10),
+            if (widget.questions![currentQuestion].answers.answerD != null)
+              _buildAnswerToggleButtons(
+                  widget.questions![currentQuestion].answers.answerD,
+                  isAnswerCorrect[3],
+                  (value) => setState(() => isAnswerCorrect[3] = value)),
+            const SizedBox(height: 10),
+            if (widget.questions![currentQuestion].answers.answerE != null)
+              _buildAnswerToggleButtons(
+                  widget.questions![currentQuestion].answers.answerE,
+                  isAnswerCorrect[4],
+                  (value) => setState(() => isAnswerCorrect[4] = value)),
+            const SizedBox(height: 10),
+            if (widget.questions![currentQuestion].answers.answerF != null)
+              _buildAnswerToggleButtons(
+                  widget.questions![currentQuestion].answers.answerF,
+                  isAnswerCorrect[5],
+                  (value) => setState(() => isAnswerCorrect[5] = value)),
+            const SizedBox(height: 10),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            ElevatedButton(
+              onPressed: () => showAnswers(),
+              child: const Text("Show answers"),
+            ),
+            if (currentQuestion + 1 < widget.questions!.length)
+              ElevatedButton(
+                onPressed: () => nextQuestion(),
+                child: const Text("Next"),
+              )
+            else
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => const HomePage()));
+                  },
+                  child: const Text("End"))
+          ],
+        )
+      ]),
+    );
+  }
+
+  Widget _buildAnswerToggleButtons(
+      String answer, bool isSelected, ValueChanged<bool> onPressed) {
+    return StatefulBuilder(builder: (context, setState) {
+      return ToggleButtons(
+        onPressed: (index) => onPressed(!isSelected),
+        isSelected: [isSelected],
+        children: [Flexible(child: Text(answer))],
+      );
+    });
+  }
+}
