56228e09a27d0329483247b68aa93e727cd254bb
studAlexP <alexander.pruka@stud.fh-campuswien.ac.at>
2023-04-14 09:45:22 +0200

    moved themes into seperate folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:quiz_it_flutter/presentation/screens/home/home_screen.dart';
+import 'package:quiz_it_flutter/presentation/themes/theme.dart';
 
 Future main() async {
   await dotenv.load(fileName: ".env");
@@ -10,150 +11,12 @@ Future main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30)
-            )
-          )
-        ),
-
-        inputDecorationTheme: InputDecorationTheme(
-          border: OutlineInputBorder(
-            borderSide: const BorderSide(color: Colors.green, width: 2),
-            borderRadius: BorderRadius.circular(30)
-          ),
-
-          enabledBorder: OutlineInputBorder(
-            borderSide: const BorderSide(color: Colors.green, width: 2),
-            borderRadius: BorderRadius.circular(30)
-          ),
-
-          focusedBorder: OutlineInputBorder(
-            borderSide: const BorderSide(color: Colors.green, width: 4),
-            borderRadius: BorderRadius.circular(30)
-          )
-        ),
-
-        outlinedButtonTheme: OutlinedButtonThemeData(
-          style: OutlinedButton.styleFrom(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30)
-            ),
-            side: const BorderSide(color: Colors.green, width: 2)
-          )  
-        ),
-
-        textTheme: const TextTheme(
-          titleLarge: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-          titleMedium: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          titleSmall: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-          displayLarge: TextStyle(fontSize: 25, color: Colors.black),
-          displayMedium: TextStyle(fontSize: 20, color: Colors.black),
-          displaySmall: TextStyle(fontSize: 15, color: Colors.black)
-        ),
-
-        cardTheme: CardTheme(
-          elevation: 10,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30),
-          side: const BorderSide(color: Colors.green, width: 3)),
-        ),
-
-        appBarTheme: const AppBarTheme(
-          elevation: 3,
-          titleTextStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.normal)
-        ),
-
-        toggleButtonsTheme: ToggleButtonsThemeData(
-          constraints: const BoxConstraints(
-            minWidth: 350,
-            minHeight: 60,
-            //maxHeight: 60,
-            maxWidth: 350
-          ),
-          borderRadius: BorderRadius.circular(30)
-        )
-
-      ),
-      darkTheme: ThemeData.dark().copyWith(
-        colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: Colors.green
-        ),
-
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30)
-            )
-          )
-        ),
-
-        inputDecorationTheme: InputDecorationTheme(
-          border: OutlineInputBorder(
-            borderSide: const BorderSide(color: Colors.green, width: 2),
-            borderRadius: BorderRadius.circular(30)
-          ),
-
-          enabledBorder: OutlineInputBorder(
-            borderSide: const BorderSide(color: Colors.green, width: 2),
-            borderRadius: BorderRadius.circular(30)
-          ),
-
-          focusedBorder: OutlineInputBorder(
-            borderSide: const BorderSide(color: Colors.green, width: 4),
-            borderRadius: BorderRadius.circular(30)
-          )
-        ),
-
-        outlinedButtonTheme: OutlinedButtonThemeData(
-          style: OutlinedButton.styleFrom(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30)
-            ),
-            side: const BorderSide(color: Colors.green, width: 2)
-          )  
-        ),
-
-        textTheme: const TextTheme(
-          titleLarge: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-          titleMedium: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          titleSmall: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-          displayLarge: TextStyle(fontSize: 25, color: Colors.white),
-          displayMedium: TextStyle(fontSize: 20, color: Colors.white),
-          displaySmall: TextStyle(fontSize: 15, color: Colors.white)
-        ),
-
-        cardTheme: CardTheme(
-          elevation: 10,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30),
-          side: const BorderSide(color: Colors.green, width: 3)),
-        ),
-
-        appBarTheme: const AppBarTheme(
-          elevation: 3,
-          titleTextStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.normal)
-        ),
-
-        toggleButtonsTheme: ToggleButtonsThemeData(
-          constraints: const BoxConstraints(
-            minWidth: 350,
-            minHeight: 60,
-            //maxHeight: 60,
-            maxWidth: 350
-          ),
-          borderRadius: BorderRadius.circular(30),
-          borderColor: Colors.white
-        )
-
-      ),
+      theme: DefaultTheme.lightTheme,
+      darkTheme: DefaultTheme.darkTheme,
       themeMode: ThemeMode.system,
       home: const HomePage()
     );
---
+++ lib/presentation/themes/theme.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+
+class DefaultTheme {
+  static final lightTheme = ThemeData(
+    primarySwatch: Colors.green,
+
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30)
+        )
+      )
+    ),
+
+    inputDecorationTheme: InputDecorationTheme(
+      border: OutlineInputBorder(
+        borderSide: const BorderSide(color: Colors.green, width: 2),
+        borderRadius: BorderRadius.circular(30)
+      ),
+
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+        borderSide: const BorderSide(color: Colors.green, width: 2)
+      ),
+
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+        borderSide: const BorderSide(color: Colors.green, width: 4)
+      )
+    ),
+
+    outlinedButtonTheme: OutlinedButtonThemeData(
+      style: OutlinedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30)
+        ),
+        side: const BorderSide(color: Colors.green, width: 2)
+      )
+    ),
+
+    textTheme: const TextTheme(
+      titleLarge: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+      titleMedium: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      titleSmall: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+      displayLarge: TextStyle(fontSize: 25, color: Colors.black),
+      displayMedium: TextStyle(fontSize: 20, color: Colors.black),
+      displaySmall: TextStyle(fontSize: 15, color: Colors.black)
+    ),
+
+    cardTheme: CardTheme(
+      elevation: 10,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30),
+        side: const BorderSide(color: Colors.green, width: 3)
+      )
+    ),
+
+    appBarTheme: const AppBarTheme(
+      elevation: 3,
+      titleTextStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.normal)
+    ),
+
+    toggleButtonsTheme: ToggleButtonsThemeData(
+      constraints: const BoxConstraints(
+        minWidth: 350,
+        maxWidth: 350,
+        minHeight: 60
+      ),
+      borderRadius: BorderRadius.circular(30)
+    )
+  );
+
+  static final darkTheme = ThemeData(
+    primarySwatch: Colors.green,
+
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30)
+        )
+      )
+    ),
+
+    inputDecorationTheme: InputDecorationTheme(
+      border: OutlineInputBorder(
+        borderSide: const BorderSide(color: Colors.green, width: 2),
+        borderRadius: BorderRadius.circular(30)
+      ),
+
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+        borderSide: const BorderSide(color: Colors.green, width: 2)
+      ),
+
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+        borderSide: const BorderSide(color: Colors.green, width: 4)
+      )
+    ),
+
+    outlinedButtonTheme: OutlinedButtonThemeData(
+      style: OutlinedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30)
+        ),
+        side: const BorderSide(color: Colors.green, width: 2)
+      )
+    ),
+
+    textTheme: const TextTheme(
+      titleLarge: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+      titleMedium: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      titleSmall: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+      displayLarge: TextStyle(fontSize: 25, color: Colors.white),
+      displayMedium: TextStyle(fontSize: 20, color: Colors.white),
+      displaySmall: TextStyle(fontSize: 15, color: Colors.white)
+    ),
+
+    cardTheme: CardTheme(
+      elevation: 10,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30),
+        side: const BorderSide(color: Colors.green, width: 3)
+      )
+    ),
+
+    appBarTheme: const AppBarTheme(
+      elevation: 3,
+      titleTextStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.normal)
+    ),
+
+    toggleButtonsTheme: ToggleButtonsThemeData(
+      constraints: const BoxConstraints(
+        minWidth: 350,
+        maxWidth: 350,
+        minHeight: 60
+      ),
+      borderRadius: BorderRadius.circular(30),
+      borderColor: Colors.white
+    )
+  );
+}
