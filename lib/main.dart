5e326293929c307645514b3d260d14de4ee15caf
studAlexP <alexander.pruka@stud.fh-campuswien.ac.at>
2023-04-14 09:06:25 +0200

    added dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -87,11 +87,72 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSwatch(
           primarySwatch: Colors.green
         ),
+
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30)
+            )
+          )
+        ),
+
         inputDecorationTheme: InputDecorationTheme(
           border: OutlineInputBorder(
+            borderSide: const BorderSide(color: Colors.green, width: 2),
+            borderRadius: BorderRadius.circular(30)
+          ),
+
+          enabledBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color: Colors.green, width: 2),
+            borderRadius: BorderRadius.circular(30)
+          ),
+
+          focusedBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color: Colors.green, width: 4),
             borderRadius: BorderRadius.circular(30)
           )
+        ),
+
+        outlinedButtonTheme: OutlinedButtonThemeData(
+          style: OutlinedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30)
+            ),
+            side: const BorderSide(color: Colors.green, width: 2)
+          )  
+        ),
+
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+          titleMedium: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          titleSmall: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+          displayLarge: TextStyle(fontSize: 25, color: Colors.white),
+          displayMedium: TextStyle(fontSize: 20, color: Colors.white),
+          displaySmall: TextStyle(fontSize: 15, color: Colors.white)
+        ),
+
+        cardTheme: CardTheme(
+          elevation: 10,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30),
+          side: const BorderSide(color: Colors.green, width: 3)),
+        ),
+
+        appBarTheme: const AppBarTheme(
+          elevation: 3,
+          titleTextStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.normal)
+        ),
+
+        toggleButtonsTheme: ToggleButtonsThemeData(
+          constraints: const BoxConstraints(
+            minWidth: 350,
+            minHeight: 60,
+            //maxHeight: 60,
+            maxWidth: 350
+          ),
+          borderRadius: BorderRadius.circular(30),
+          borderColor: Colors.white
         )
+
       ),
       themeMode: ThemeMode.system,
       home: const HomePage()
