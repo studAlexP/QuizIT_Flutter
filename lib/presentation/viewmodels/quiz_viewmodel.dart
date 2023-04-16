74218a3b33017baf5e77abb5df9804f299cc99e9
studAlexP <alexander.pruka@stud.fh-campuswien.ac.at>
2023-04-16 16:24:42 +0200

    adapted imports & naming

--- lib/presentation/viewmodels/quiz_viewmodel.dart
+++ lib/presentation/viewmodels/quiz_viewmodel.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_it_flutter/data/network/quiz_api_impl.dart';
+import 'package:quiz_it_flutter/services/quiz_api.dart';
 import 'package:quiz_it_flutter/models/question.dart';
 
 class QuizViewModel extends ChangeNotifier {
-  final QuizApiImpl _quizApiImpl = QuizApiImpl();
+  final QuizApi _quizApi = QuizApi();
 
   List<Question>? _questions;
 
   List<Question>? get questions => _questions;
 
   getQuestions({required String category, required String difficulty, required String limit}) async {
-    _questions = await _quizApiImpl.getQuestions(category: category, difficulty: difficulty, limit: limit);
+    _questions = await _quizApi.getQuestions(category: category, difficulty: difficulty, limit: limit);
     notifyListeners();
   }
 }
